01554ce9978798bf1aa82a8aba431da6a10c0d08
Willem Veelenturf <willem.veelenturf@gmail.com>
2023-02-22 17:56:47 +0100

    RM 1.4-compat

--- src/sbt-test/kotlin/kotlin-1.4-compat/src/main/kotlin/simple.kt
+++
@@ -1,7 +0,0 @@
-package demo
-fun main(args: Array<String>) {
-  // Test some Kotlin 1.3 features
-  val keys = 'a'..'f'
-  val map = keys.associateWith { it.toString().repeat(5).capitalize() }
-  map.forEach { println(it) }
-}
